7d3607788b7ee507aa48962cd8db9a85a53d8bc0
Chandan Singh <chandan.devel@gmail.com>
2014-12-09 20:31:19 +0530

    Add basic listing of movies

--- starter/views.py
+++ starter/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+import urllib2
+import json
 
 def landing_page(request):
-    return render(request, "starter/landing_page.html")
+    raw_data = urllib2.urlopen("http://127.0.0.1:8000/static/data/movies.json").read()
+    data = json.loads(raw_data)
+    movies = data['movies']
+    return render(request, "starter/landing_page.html", {"movies": movies})
