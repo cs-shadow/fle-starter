5f562d05b2c633f37cf672297c8c4ca2a593216f
Chandan Singh <chandan.devel@gmail.com>
2014-12-10 21:06:38 +0530

    Add basic search feature

--- starter/views.py
+++ starter/views.py
@@ -5,5 +5,11 @@ import json
 def landing_page(request):
     raw_data = urllib2.urlopen("http://127.0.0.1:8000/static/data/movies.json").read()
     data = json.loads(raw_data)
-    movies = data['movies']
+    all_movies = data['movies']
+    movies = []
+    search_query = request.GET['search'] if 'search' in request.GET else ""
+    # Filter movies containing the search string.
+    for movie in all_movies:
+        if search_query.lower() in movie['title'].lower():
+            movies.append(movie)
     return render(request, "starter/landing_page.html", {"movies": movies})
